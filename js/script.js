e2144ada131e422a61210c50b3cb966d3fba4061
JunoSaffron <37506194+JunoSaffron@users.noreply.github.com>
2022-02-26 10:08:45 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -75,3 +75,25 @@ var swiper = new Swiper(".review-slider", {
         },
     },
 });
+
+
+
+var swiper = new Swiper(".image-slider", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 2,
+        slideShadows: true,
+    },
+    loop:true,
+    autoplay: {
+          delay: 2000,
+          disableOnInteraction: false,
+    },
+});    
+
